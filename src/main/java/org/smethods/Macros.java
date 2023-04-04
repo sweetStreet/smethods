aa4f74ea3e662307989ca66ec84b423c21541950
Yuki Liu <yuki.liu@utexas.edu>
2023-04-04 11:36:46 -0500

    remove not used variables

--- src/main/java/org/smethods/Macros.java
+++ src/main/java/org/smethods/Macros.java
@@ -1,13 +1,6 @@
 package org.smethods;
 
 public class Macros {
-    static String projectFolder = "ekstazi_projects";
-    static String projectFolderPath = System.getProperty("user.home") + "/" + projectFolder;
-    static String resultFolder = "result";
-    static String resultFolderPath = projectFolderPath + "/" + resultFolder;
-    static int numSHA = 1;
-    static String SKIPS = " -Djacoco.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip " +
-            "-Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dpmd.skip -Dxjc.skip";
     public static final String SMETHODS_ROOT_DIR_NAME = ".smethods";
     public static String PROJECT_PACKAGE = "org/smethods/";
 }
